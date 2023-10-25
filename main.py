af499234643f5043a6e28bf6d5cd5f945aeb7376
matiasjrodriguez <mellarodriguez29@gmail.com>
2023-10-25 15:44:23 -0300

    definición inicial de clase App

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+
+class App:
+    def __init__(self):
+        self.width = 800
+        self.height = 450
+        self.res = self.width, self.height
+        self.screen = pygame.display.set_mode(self.res)
+        self.clock = pygame.time.clock()
