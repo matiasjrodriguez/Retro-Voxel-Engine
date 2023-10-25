d0fb3f511b724cb68841b422c59998524dea2a65
matiasjrodriguez <mellarodriguez29@gmail.com>
2023-10-25 17:08:24 -0300

    se agrega método update

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+import pygame
 import numpy
 import math
 
@@ -10,3 +11,35 @@ class Player:
         self.angle_vel = 0.01
         self.vel = 3
         
+    def update(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_UP]:
+            self.pitch += self.vel
+        if pressed_key[pygame.K_DOWN]:
+            self.pitch -= self.vel
+
+        if pressed_key[pygame.K_LEFT]:
+            self.angle -= self.angle_vel
+        if pressed_key[pygame.K_RIGHT]:
+            self.angle += self.angle_vel
+
+        if pressed_key[pygame.K_q]:
+            self.height += self.vel
+        if pressed_key[pygame.K_e]:
+            self.height -= self.vel
+
+        if pressed_key[pygame.K_w]:
+            self.pos[0] += self.vel * cos_a
+            self.pos[1] += self.vel * sin_a
+        if pressed_key[pygame.K_s]:
+            self.pos[0] -= self.vel * cos_a
+            self.pos[1] -= self.vel * sin_a
+        if pressed_key[pygame.K_a]:
+            self.pos[0] += self.vel * sin_a
+            self.pos[1] -= self.vel * cos_a
+        if pressed_key[pygame.K_d]:
+            self.pos[0] -= self.vel * sin_a
+            self.pos[1] += self.vel * cos_a
